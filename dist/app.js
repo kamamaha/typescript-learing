a9fd43eb31f3b2e7f0f0fdd6bdfe2a38ece83b24
Kamila <kamilagrzadko@gmail.com>
2023-08-30 12:34:52 +0200

    basic2

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,18 @@
-// const input1Element: HTMLInputElement = document.querySelector("#input1")
-// const input2Element: HTMLInputElement = document.querySelector("#input2")
-// const addButtonElement = document.querySelector("button")
-// const add = (v1: number, v2: number) => v1 + v2;
-// addButtonElement.addEventListener("click", () => {
-//     const sum = add(Number(input1Element.value), Number(input2Element.value))
-//     console.log(sum)
-// })
+const input1Element = document.querySelector("#input1");
+const input2Element = document.querySelector("#input2");
+const addButtonElement = document.querySelector("button");
+const add = (v1, v2) => v1 + v2;
+addButtonElement.addEventListener("click", () => {
+    const sum = add(Number(input1Element.value), Number(input2Element.value));
+    console.log(sum);
+});
+const LogAge = (age) => {
+    return (console.log(`hej mam ${age} lat`));
+};
+LogAge(30);
+LogAge("dwa");
+let test;
+test = "test";
+test = 2;
+test = true;
+test = [];
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,27 @@
-// const input1Element: HTMLInputElement = document.querySelector("#input1")
-// const input2Element: HTMLInputElement = document.querySelector("#input2")
-// const addButtonElement = document.querySelector("button")
+const input1Element: HTMLInputElement = document.querySelector("#input1")
+const input2Element: HTMLInputElement = document.querySelector("#input2")
+const addButtonElement = document.querySelector("button")
 
-// const add = (v1: number, v2: number) => v1 + v2;
+const add = (v1: number, v2: number) => v1 + v2;
 
-// addButtonElement.addEventListener("click", () => {
-//     const sum = add(Number(input1Element.value), Number(input2Element.value))
-//     console.log(sum)
-// })
+addButtonElement.addEventListener("click", () => {
+    const sum = add(Number(input1Element.value), Number(input2Element.value))
+    console.log(sum)
+})
+
+const LogAge = (age: number | string) => {
+    return(
+        console.log(`hej mam ${age} lat`)
+    )
+    
+}
+
+LogAge(30)
+LogAge("dwa")
+
+let test: number | string | boolean | []
+
+test = "test"
+test = 2
+test = true
+test = []
