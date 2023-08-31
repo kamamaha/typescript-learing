7286e95e9c208db9aa0798d10735f4735c7c44c7
Kamila <kamilagrzadko@gmail.com>
2023-08-31 11:22:55 +0200

    todo app

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,48 @@
-const buttonElement = document.querySelector("button");
-const calculatePrice = (originalPrice, hasDiscount) => {
-    return hasDiscount ? originalPrice * 0.8 : originalPrice;
+const taskNameInputElemenet = document.querySelector("#name");
+const addButtonElement = document.querySelector("button");
+const tasksContainerElement = document.querySelector(".tasks");
+const tasks = [
+    {
+        title: "Wyrzucić śmieci",
+        done: false,
+    },
+    {
+        title: "Pójść do sklepu",
+        done: true,
+    },
+    {
+        title: "Nakarmić psa",
+        done: false,
+    },
+];
+const render = () => {
+    tasksContainerElement.innerHTML = "";
+    tasks.forEach((task, index) => {
+        const taskElement = document.createElement("li");
+        const id = `task-${index}`;
+        const labelElement = document.createElement("label");
+        labelElement.innerText = task.title;
+        labelElement.setAttribute("for", id);
+        const checkboxElement = document.createElement("input");
+        checkboxElement.type = "checkbox";
+        checkboxElement.name = task.title;
+        checkboxElement.id = id;
+        checkboxElement.checked = task.done;
+        checkboxElement.addEventListener("change", () => {
+            task.done = !task.done;
+        });
+        taskElement.appendChild(labelElement);
+        taskElement.appendChild(checkboxElement);
+        tasksContainerElement.appendChild(taskElement);
+    });
 };
-buttonElement.addEventListener("click", () => {
-    const originalPrice = 50;
-    const hasDiscount = new URLSearchParams(window.location.search).get("discount") === "true";
-    console.log(calculatePrice(originalPrice, hasDiscount));
+const addTask = (task) => {
+    tasks.push(task);
+};
+addButtonElement.addEventListener("click", (event) => {
+    event.preventDefault();
+    addTask({ title: taskNameInputElemenet.value, done: false });
+    render();
 });
+addTask({ title: "spacjalne zadanie od szefa", done: true });
+render();
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,63 @@
-const buttonElement = document.querySelector("button")
 
-const calculatePrice = (originalPrice: number, hasDiscount: boolean) => {
-    return hasDiscount ? originalPrice * 0.8 : originalPrice
-}
 
-buttonElement.addEventListener("click", () => {
-    const originalPrice: number = 50;
-    const hasDiscount: boolean = new URLSearchParams(window.location.search).get("discount") === "true"
+const taskNameInputElemenet: HTMLInputElement = document.querySelector("#name");
+const addButtonElement: HTMLButtonElement = document.querySelector("button");
+const tasksContainerElement: HTMLElement = document.querySelector(".tasks");
 
-    console.log(calculatePrice(originalPrice, hasDiscount))
-})
+const tasks: {
+    title: string;
+  done: boolean;
+}[] = [
+  {
+    title: "Wyrzucić śmieci",
+    done: false,
+  },
+  {
+    title: "Pójść do sklepu",
+    done: true,
+  },
+  {
+    title: "Nakarmić psa",
+    done: false,
+  },
+];
+
+const render = () => {
+  tasksContainerElement.innerHTML = "";
+  tasks.forEach((task, index) => {
+
+    const taskElement: HTMLElement = document.createElement("li");
+    const id: string = `task-${index}`;
+    const labelElement: HTMLLabelElement = document.createElement("label");
+    labelElement.innerText = task.title;
+    labelElement.setAttribute("for", id)
+
+    const checkboxElement: HTMLInputElement = document.createElement("input")
+    checkboxElement.type = "checkbox";
+    checkboxElement.name = task.title; 
+    checkboxElement.id = id;
+    checkboxElement.checked = task.done;
+    checkboxElement.addEventListener("change", () => {
+        task.done = !task.done
+    })
+
+    taskElement.appendChild(labelElement)
+    taskElement.appendChild(checkboxElement)
+
+    tasksContainerElement.appendChild(taskElement)
+  });
+};
+
+const addTask = (task: {title: string, done: boolean}) => {
+  tasks.push(task);
+};
+
+addButtonElement.addEventListener("click", (event: Event) => {
+  event.preventDefault();
+  addTask({title: taskNameInputElemenet.value, done: false});
+  render();
+});
+
+addTask({title: "spacjalne zadanie od szefa", done: true})
+
+render();
